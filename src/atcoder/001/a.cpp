2587279cff0b4a9b30e2ec4d5c3e75ba5bb35dc7
Shun Kakinoki <shunkakinoki@gmail.com>
2019-10-29 16:00:56 +0900

    added auto approve with mergify

--- src/atcoder/001/a.cpp
+++ src/atcoder/001/a.cpp
@@ -1,9 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    int a, b;
-    cin >> a >> b;
-    cout << a - b << endl;
+int main() {
+  int a, b;
+  cin >> a >> b;
+  cout << a - b << endl;
 }
