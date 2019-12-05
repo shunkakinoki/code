6d24e8167db6b27c4672229d1ce1083a17a6159a
Shun Kakinoki <shunkakinoki@gmail.com>
2019-12-05 12:59:37 +0900

    init 12 05

---
+++ cpp/try/hackerrank/introduction/variable_sized_arrays.cpp
@@ -0,0 +1,13 @@
+// Copyright 2019 Shun Kakinoki.
+
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+  return 0;
+}
